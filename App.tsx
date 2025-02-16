246604be595747c50eddb12be83a6531a122e9a6
quyen3823 <quyenngob2k3@gmail.com>
2025-02-16 21:29:56 +0700

    28

--- App.tsx
+++ App.tsx
@@ -1,16 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello word</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>Test</Text>
+      </View>
+      <View>
+        <Text style={styles.text}>Hello Word</Text>
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: "red"
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
