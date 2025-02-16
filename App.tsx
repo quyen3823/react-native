85fd9159b00fd95bcbb749ac60dfa62e2442029d
quyen3823 <quyenngob2k3@gmail.com>
2025-02-16 22:28:09 +0700

    30

--- App.tsx
+++ App.tsx
@@ -1,24 +1,42 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import {useState} from 'react';
 
 export default function App() {
   const [name, setName] = useState<string>("react native");
-  const [age, setAge] = useState<number>(0);
-  const [person, setPerson] = useState<{
-    name: string;
-    age: number;
-  }>({
-    name: "Quyen Quyen",
-    age: 25
 
-  })
+
+
+  // const [age, setAge] = useState<number>(0);
+  // const [person, setPerson] = useState<{
+  //   name: string;
+  //   age: number;
+  // }>({
+  //   name: "Quyen Quyen",
+  //   age: 25
+
+  // })
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.text}>{JSON.stringify(person)}</Text>
-        <Text style={styles.text}>{age}</Text>
+        {/* <Text style={styles.text}>{JSON.stringify(person)}</Text>
+        <Text style={styles.text}>{age}</Text> */}
+        <TextInput
+        onChangeText={ v => setName(v)}
+        value={name}
+         autoCapitalize='none'
+         autoCorrect={false}
+        //  keyboardType='numeric'
+        //  maxLength={2}
+        multiline={true}
+         style={{
+          borderColor: "violet",
+          borderWidth: 1,
+          padding: 10
+          }} />
+          <Text style={styles.text} >{name}</Text>
       </View>
+      <Button title='Add new' />
       <View>
         <Text style={styles.text}>Hello Word</Text>
       </View>
